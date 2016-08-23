c62b970e1113ca08057505ae6977b173c6011ff6
JinYiliang <1246505492@qq.com>
2016-08-23 14:12:20 +0800

    scale

--- canvas/js/canvasbase.js
+++ canvas/js/canvasbase.js
@@ -1,9 +1,22 @@
 var canvas = document.getElementById('canvas-sample');
 var ctx = canvas.getContext('2d');
-
-ctx.lineWidth = 10;
-ctx.strokStyle = 'red';
+//画矩形
+ctx.lineWidth = 1;
+ctx.strokeStyle = "#ff0000";
+ctx.fillStyle = "#ff0000"
 ctx.beginPath();
-ctx.moveTo(10,10);
-ctx.lineTo(150,50);
-ctx.stroke();
+// ctx.lineCap = "square";
+// ctx.lineCap = "butt";
+// ctx.lineCap = "round";
+// ctx.strokeRect(10,10,70,70);
+
+//画圆形
+ctx.beginPath();
+ctx.arc(50,50,30,0,90*Math.PI/180,false);
+ctx.fill();
+
+//画文字
+ctx.beginPath();
+ctx.font = "italic lighter 30px Arial";
+ctx.fillText("你好",50,50);
+// ctx.strokeText("Hello,world",50,50);
